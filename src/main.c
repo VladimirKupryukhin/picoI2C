3faa6cef96031a6c51fc68ce2f1a9318169c4a8b
Vladimir Kupryukhin <vladimirkupryukhin@gmail.com>
2024-02-16 12:03:01 -0800

    Basic Setup Code. The light blinks

---
+++ src/main.c
@@ -0,0 +1,19 @@
+#include "pico/stdlib.h"
+#include <stdio.h>
+
+int main () {
+    stdio_init_all();
+
+    gpio_init(25);
+    gpio_set_dir(25, GPIO_OUT);
+
+    for (int index = 0; index < 10; index++) {
+        gpio_put(25,index % 2);
+        sleep_ms(1000);
+        printf("%d \n", index);
+    }
+
+
+
+    return 0;
+}
